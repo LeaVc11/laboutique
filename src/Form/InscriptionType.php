98bf2aaaf9c2aa95bb16b6bf744fd5995fcf29cc
Carine <vinagre.carine@gmail.com>
2021-10-06 01:09:04 +0200

    formulaires inscription

---
+++ src/Form/InscriptionType.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InscriptionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom',TextType::class,
+                [
+                    'label'=>'Votre nom',
+                    'attr'=>
+                        [
+                            'placeholder'=>'Merci de saisir votre nom'
+                        ]
+                ])
+            ->add('prenom',TextType::class,
+                [
+                    'label'=>'Votre prénom',
+                    'attr'=>
+                        [
+                            'placeholder'=>'Merci de saisir votre prénom'
+                        ]
+                ])
+            ->add('email', EmailType::class,
+                [
+                    'label'=>'Votre email',
+                    'attr'=>
+                        [
+                            'placeholder'=>'Merci de saisir votre email'
+                        ]
+                ])
+            ->add('password',PasswordType::class,
+                [
+                    'label'=>'Votre mot de passe',
+                    'attr'=>
+                        [
+                            'placeholder'=>'Merci de saisir votre mot de passe'
+                        ]
+                ])
+            -> add('password_confirm',PasswordType::class,
+                [
+                    'label'=>'Confirmer votre de mot de passe',
+                    'mapped'=>false,
+                    'attr'=>
+                        [
+                            'placeholder'=>'Confirmer votre prénom'
+                        ]
+                ])
+            ->add('submit',SubmitType::class,
+                [
+                    'label'=>"S'inscrire"
+                ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
