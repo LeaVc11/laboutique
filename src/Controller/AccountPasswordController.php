7d06c48a25d37492ed47d7059d13ffe8b451b466
Carine <vinagre.carine@gmail.com>
2021-10-06 17:29:34 +0200

    cnew password

---
+++ src/Controller/AccountPasswordController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ChangePasswordType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccountPasswordController extends AbstractController
+{
+    #[Route('/compte/modifier-password', name: 'account_password')]
+    public function index()
+    {
+        $user=$this->getUser();
+        $form=$this->createForm(ChangePasswordType::class,$user);
+        return $this->render('account/password.html.twig',
+        [
+            'form'=> $form->createView()
+        ]);
+    }
+}
