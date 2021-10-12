9be15d0fd30b4bae57f5c8abde364b186b546a42
Carine <vinagre.carine@gmail.com>
2021-10-12 13:07:57 +0200

    Ccréer la vue panier

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -3,19 +3,45 @@
 namespace App\Controller;
 
 use App\Classe\Panier;
+use App\Entity\Produit;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PanierController extends AbstractController
 {
+
+    private $entityManager;
+
+    /**
+     * @param $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+
     #[Route('/mon-panier', name: 'panier')]
     public function index(Panier $panier)
     {
 
-        dd($panier->get());
+        /*dd($panier->get());*/
+        $panierComplet = [];
 
-        return $this->render('panier/index.html.twig');
+        foreach ($panier->get() as $id => $quantity) {
+            $panierComplet[] =
+                [
+                    'product' => $this->entityManager->getRepository(Produit::class)->findOneById($id),
+                    'quantity' => $quantity
+                ];
+        }
+/*        dd($panierComplet);*/
+
+        return $this->render('panier/index.html.twig',
+            [
+                'panier' => $panierComplet
+            ]);
     }
 
     #[Route('/panier/add/{id}', name: 'add_to_panier')]
