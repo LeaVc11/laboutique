85c53877530f3658083ce0a096736e2b0b1788c6
Carine V <77583125+LeaVc11@users.noreply.github.com>
2023-04-04 10:42:11 +0200

    modification

--- src/Entity/SupprimerPassword.php
+++ src/Entity/SupprimerPassword.php
@@ -13,7 +13,7 @@ class SupprimerPassword
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
+     * @ORM\Column(type="integer")create
      */
     private $id;
 
